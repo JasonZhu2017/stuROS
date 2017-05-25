da3d17105d93ecd8c364fe76232ce9fffa116e56
JasonZhu2017 <zhujh_acc@126.com>
2017-05-25 21:34:44 +0800

    add agitr

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/zjh/zBox/sync/projects/stuROS/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/zjh/zBox/sync/projects/stuROS/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/zjh/zBox/sync/projects/stuROS/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD="/home/zjh/zBox/sync/projects/stuROS/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/zjh/zBox/sync/projects/stuROS/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/zjh/zBox/sync/projects/stuROS/src:$ROS_PACKAGE_PATH"
